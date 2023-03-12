d03c7f83e3b140882c6b4495e2cdb1908224caea
Jason Dieter Oesch <64602559+JamesArthemeusFinnigan@users.noreply.github.com>
2023-03-12 15:55:03 +0100

    higher res

--- Images/ImagesBackend/Video/ConvertTowebm.zsh
+++ Images/ImagesBackend/Video/ConvertTowebm.zsh
@@ -1,5 +1,5 @@
 for f in *.mkv;
 do
 ffmpeg -i $f -filter_complex "[0:v] palettegen" ${f%palette}.png
-ffmpeg -i $f -i ${f%palette}.png -filter_complex "[0:v] fps=24,scale=500:-1 [new];[new][1:v] paletteuse" ${f}.gif
+ffmpeg -i $f -i ${f%palette}.png -filter_complex "[0:v] fps=24,scale=1000:-1 [new];[new][1:v] paletteuse" ${f}.gif
 done
