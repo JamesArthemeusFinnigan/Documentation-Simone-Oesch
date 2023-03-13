76aa324616e27e5d5d0c23ba17c1850b14c12b21
Jason Dieter Oesch <64602559+JamesArthemeusFinnigan@users.noreply.github.com>
2023-03-13 18:26:53 +0100

    Uploading Teams Explainer

--- Images/ImagesBackend/Video/ConvertTowebm.zsh
+++ Images/ImagesBackend/Video/ConvertTowebm.zsh
@@ -1,7 +1,7 @@
 for f in *.mkv;
 do
 ffmpeg -i $f -filter_complex "[0:v] palettegen" ${f%palette}.png
-ffmpeg -i $f -ss 3 -c copy output.mp4 ${f%palette}.png -filter_complex "[0:v] fps=24,scale=500:-1 [new];[new][1:v] paletteuse" ${f}.gif
+ffmpeg -i $f -i ${f%palette}.png -filter_complex "[0:v] fps=24,scale=500:-1 [new];[new][1:v] paletteuse" ${f}.gif
 done
 
 
